b6ad2782ab18aa339e8d06151acc081798939682
emir14akbasoglu <eakba7197@gmail.com>
2021-09-28 14:54:04 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("ANGARA +IC ANADOLU");
+     System.out.println("ISTANBUL>>MARMARA");
+     System.out.println("VAN>>DOGU ANDOLU");
+     System.out.println("BOLU>>BATIKARADENÝZ");
+     System.out.println("NEVSEHIR>>IC ANADOLU");
+     System.out.println("DÜZCE>> BATI KARADENIZ");
+     
+     
+	}
+
+}
